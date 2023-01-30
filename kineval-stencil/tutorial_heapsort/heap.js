41c813065111f5f6c30941bc4ef29432a74ac11e
servando-g <48229452+servando-g@users.noreply.github.com>
2023-01-30 16:44:19 -0500

    Modified minheap_extract and minheap_insert

--- kineval-stencil/tutorial_heapsort/heap.js
+++ kineval-stencil/tutorial_heapsort/heap.js
@@ -29,6 +29,26 @@ minheaper = {};
 function minheap_insert(heap, new_element) {
 
     // STENCIL: implement your min binary heap insert operation
+    // push() places the new element at the end of the array
+    heap.push(new_element);
+
+    // The last index of heap
+    let current_index = heap.length - 1;
+
+    while (current_index > 0) {
+        let parent_index = Math.round(current_index/2) - 1;
+        if (heap[parent_index] > heap[current_index]) {
+            let temp = heap[current_index];
+            heap[current_index] = heap[parent_index];
+            heap[parent_index] = temp;
+            current_index = parent_index;
+        }
+        else {
+            break;
+        }
+    }
+
+    
 }
 
 // assign insert function within minheaper object
@@ -41,15 +61,46 @@ minheaper.insert = minheap_insert;
 // define extract function for min binary heap
 function minheap_extract(heap) {
 
+    if (heap.length == 0) {
+        return;
+    }
+    else if (heap.length == 1) {
+        return heap.pop()
+    }
+
     // STENCIL: implement your min binary heap extract operation
+    let first_index = heap[0];
+    heap[0] = heap.pop();
+
+    var current_index = 0;
+
+    var left_index = (current_index * 2) + 1;
+    var right_index = (current_index * 2) + 2;
+
+    while (heap[current_index] > heap[left_index] || heap[current_index] > heap[right_index]) {
+        let index = left_index;
+        if (heap[left_index] > heap[right_index]) {
+            index = right_index;
+        }
+
+        if (heap[current_index] > heap[index]) {
+            let temp = heap[current_index];
+            heap[current_index] = heap[index];
+            heap[index] = temp;
+            current_index = index;
+            left_index = (current_index * 2) + 1;
+            right_index = (current_index * 2) + 2;
+        } 
+    }
+
+    return first_index
+
 }
 
 // assign extract function within minheaper object
-
+minheaper.extract = minheap_extract;
     // STENCIL: ensure extract method is within minheaper object
-
-
-
-
+console.log(minheaper.insert)
+console.log(minheaper.extract)
 
 
