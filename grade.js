a6a8416a8f8e697307ecb4bb70e10ba0f4e11dac
Liz Olson <lizolson@umich.edu>
2023-04-01 02:15:32 -0400

    Update grades at time: 01-04-2023-02-02-42

---
+++ grade.js
@@ -0,0 +1,386 @@
+//CI grader generates report at 01-04-2023-02-02-40
+//Commit hash: 45df820
+grade = 
+{
+  "servando": {
+    "name": "Garza, Servando",
+    "unique_name": "servando",
+    "email": "servando@umich.edu",
+    "course": "ROB 320/EECS 367 W23",
+    "repository": "servando-g/kineval-stencil",
+    "assignments": {
+      "Honor": {
+        "status": "CHECK",
+        "comments": [
+          "comment: honor code certified"
+        ]
+      },
+      "ROS Pub/Sub": {
+        "status": "CHECK",
+        "comments": "Passing 3 out of 3 tests"
+      },
+      "PathPlan_Heap": {
+        "status": "PASS",
+        "comments": [
+          "10 out of 10 test cases passed"
+        ],
+        "test results": {
+          "Test 0": {
+            "Status": "Succeed",
+            "Stderr": "b''"
+          },
+          "Test 1": {
+            "Status": "Succeed",
+            "Stderr": "b''"
+          },
+          "Test 2": {
+            "Status": "Succeed",
+            "Stderr": "b''"
+          },
+          "Test 3": {
+            "Status": "Succeed",
+            "Stderr": "b''"
+          },
+          "Test 4": {
+            "Status": "Succeed",
+            "Stderr": "b''"
+          },
+          "Test 5": {
+            "Status": "Succeed",
+            "Stderr": "b''"
+          },
+          "Test 6": {
+            "Status": "Succeed",
+            "Stderr": "b''"
+          },
+          "Test 7": {
+            "Status": "Succeed",
+            "Stderr": "b''"
+          },
+          "Test 8": {
+            "Status": "Succeed",
+            "Stderr": "b''"
+          },
+          "Test 9": {
+            "Status": "Succeed",
+            "Stderr": "b''"
+          }
+        }
+      },
+      "PathPlan_AStar": {
+        "status": "PENDING",
+        "comments": [
+          "4 out of 5 test cases passed"
+        ],
+        "test results": {
+          "Test 0": {
+            "Status": "Succeed",
+            "Stderr": "b''"
+          },
+          "Test 1": {
+            "Status": "Succeed",
+            "Stderr": "b''"
+          },
+          "Test 2": {
+            "Status": "Succeed",
+            "Stderr": "b''"
+          },
+          "Test 3": {
+            "Status": "Failed",
+            "Stderr": "Error message too long, print partially: b\"/servando/PathPlan_AStar/test_3.js:590\\n    while ((heap[current_index].priority > heap[left_index].priority || heap[current_index].priority > heap[right_index].priority)) {\\n                                                                                                                         ^\\n\\nTypeError: Cannot read property 'priority' of undefined\\n    at minheap_extract (/home/logan/Desktop/teaching/CI-grader/ci_test_root"
+          },
+          "Test 4": {
+            "Status": "Succeed",
+            "Stderr": "b''"
+          }
+        }
+      },
+      "Pendularm_Euler": {
+        "status": "PENDING",
+        "comments": [
+          "0 out of 2 test cases passed"
+        ],
+        "test results": {
+          "Test 0": {
+            "Status": "Failed",
+            "Stderr": "Error message too long, print partially: b'/servando/Pendularm_Euler/test_0.js:149\\n<<<<<<< HEAD:project_pendularm/update_pendulum_state.js\\n^^\\n\\nSyntaxError: Unexpected token <<\\n    at createScript (vm.js:80:10)\\n    at Object.runInThisContext (vm.js:139:10)\\n    at Module._compile (module.js:616:28)\\n    at Object.Module._extensions..js (module.js:663:10)\\n    at Module.load (module.js:565:32)\\n    at tryModuleLoad (module.js:505:12)\\n    at Function.Module._load (mod"
+          },
+          "Test 1": {
+            "Status": "Failed",
+            "Stderr": "Error message too long, print partially: b'/servando/Pendularm_Euler/test_1.js:149\\n<<<<<<< HEAD:project_pendularm/update_pendulum_state.js\\n^^\\n\\nSyntaxError: Unexpected token <<\\n    at createScript (vm.js:80:10)\\n    at Object.runInThisContext (vm.js:139:10)\\n    at Module._compile (module.js:616:28)\\n    at Object.Module._extensions..js (module.js:663:10)\\n    at Module.load (module.js:565:32)\\n    at tryModuleLoad (module.js:505:12)\\n    at Function.Module._load (mod"
+          }
+        }
+      },
+      "Pendularm_VelocityVerlet": {
+        "status": "PENDING",
+        "comments": [
+          "0 out of 2 test cases passed"
+        ],
+        "test results": {
+          "Test 0": {
+            "Status": "Failed",
+            "Stderr": "Error message too long, print partially: b'/servando/Pendularm_VelocityVerlet/test_0.js:149\\n<<<<<<< HEAD:project_pendularm/update_pendulum_state.js\\n^^\\n\\nSyntaxError: Unexpected token <<\\n    at createScript (vm.js:80:10)\\n    at Object.runInThisContext (vm.js:139:10)\\n    at Module._compile (module.js:616:28)\\n    at Object.Module._extensions..js (module.js:663:10)\\n    at Module.load (module.js:565:32)\\n    at tryModuleLoad (module.js:505:12)\\n    at Function.Module._"
+          },
+          "Test 1": {
+            "Status": "Failed",
+            "Stderr": "Error message too long, print partially: b'/servando/Pendularm_VelocityVerlet/test_1.js:149\\n<<<<<<< HEAD:project_pendularm/update_pendulum_state.js\\n^^\\n\\nSyntaxError: Unexpected token <<\\n    at createScript (vm.js:80:10)\\n    at Object.runInThisContext (vm.js:139:10)\\n    at Module._compile (module.js:616:28)\\n    at Object.Module._extensions..js (module.js:663:10)\\n    at Module.load (module.js:565:32)\\n    at tryModuleLoad (module.js:505:12)\\n    at Function.Module._"
+          }
+        }
+      },
+      "Pendularm_PID": {
+        "status": "PENDING",
+        "comments": [
+          "0 out of 1 test cases passed"
+        ],
+        "test results": {
+          "Test 0": {
+            "Status": "Failed",
+            "Stderr": "Error message too long, print partially: b'/servando/Pendularm_PID/test_0.js:166\\n<<<<<<< HEAD:project_pendularm/update_pendulum_state.js\\n^^\\n\\nSyntaxError: Unexpected token <<\\n    at createScript (vm.js:80:10)\\n    at Object.runInThisContext (vm.js:139:10)\\n    at Module._compile (module.js:616:28)\\n    at Object.Module._extensions..js (module.js:663:10)\\n    at Module.load (module.js:565:32)\\n    at tryModuleLoad (module.js:505:12)\\n    at Function.Module._load (modul"
+          }
+        }
+      },
+      "FK_MatrixRoutines": {
+        "status": "PASS",
+        "comments": [
+          "1 out of 1 test cases passed"
+        ],
+        "test results": {
+          "Test 0": {
+            "Status": "Succeed",
+            "Stderr": "b''"
+          }
+        }
+      },
+      "FK_Transforms": {
+        "status": "PASS",
+        "comments": [
+          "8 out of 8 test cases passed"
+        ],
+        "test results": {
+          "Test 0": {
+            "Status": "Succeed",
+            "Stderr": "b''"
+          },
+          "Test 1": {
+            "Status": "Succeed",
+            "Stderr": "b''"
+          },
+          "Test 2": {
+            "Status": "Succeed",
+            "Stderr": "b''"
+          },
+          "Test 3": {
+            "Status": "Succeed",
+            "Stderr": "b''"
+          },
+          "Test 4": {
+            "Status": "Succeed",
+            "Stderr": "b''"
+          },
+          "Test 5": {
+            "Status": "Succeed",
+            "Stderr": "Only for graduate session, skip testing"
+          },
+          "Test 6": {
+            "Status": "Succeed",
+            "Stderr": "Only for graduate session, skip testing"
+          },
+          "Test 7": {
+            "Status": "Succeed",
+            "Stderr": "Only for graduate session, skip testing"
+          }
+        }
+      },
+      "FK_JointRendering": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "FSMDance_Quaternion": {
+        "status": "PASS",
+        "comments": [
+          "1 out of 1 test cases passed"
+        ],
+        "test results": {
+          "Test 0": {
+            "Status": "Succeed",
+            "Stderr": "b''"
+          }
+        }
+      },
+      "FSMDance_RexArm": "FSMDance_RexArm",
+      "FSMDance_BaseControl": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "FSMDance_SetpointControl": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "FSMDance_FSM": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "IK_Jacobian": {
+        "status": "PASS",
+        "comments": [
+          "2 out of 2 test cases passed"
+        ],
+        "test results": {
+          "Test 0": {
+            "Status": "Succeed",
+            "Stderr": "b''"
+          },
+          "Test 1": {
+            "Status": "Succeed",
+            "Stderr": "b''"
+          }
+        }
+      },
+      "IK_JTranspose": {
+        "status": "PASS",
+        "comments": [
+          "3 out of 3 test cases passed"
+        ],
+        "test results": {
+          "Test 0": {
+            "Status": "Succeed",
+            "Stderr": "b''"
+          },
+          "Test 1": {
+            "Status": "Succeed",
+            "Stderr": "b''"
+          },
+          "Test 2": {
+            "Status": "Succeed",
+            "Stderr": "b''"
+          }
+        }
+      },
+      "IK_JPseudoInverse": {
+        "status": "PASS",
+        "comments": [
+          "3 out of 3 test cases passed"
+        ],
+        "test results": {
+          "Test 0": {
+            "Status": "Succeed",
+            "Stderr": "b''"
+          },
+          "Test 1": {
+            "Status": "Succeed",
+            "Stderr": "b''"
+          },
+          "Test 2": {
+            "Status": "Succeed",
+            "Stderr": "b''"
+          }
+        }
+      },
+      "MotionPlan_Collision": {
+        "status": "PENDING",
+        "comments": [
+          "0 out of 3 test cases passed"
+        ],
+        "test results": {
+          "Test 0": {
+            "Status": "Failed",
+            "Stderr": "b'[ERROR]: Collision test 0 failed\\n'"
+          },
+          "Test 1": {
+            "Status": "Failed",
+            "Stderr": "b'[ERROR]: Collision test 1 failed\\n'"
+          },
+          "Test 2": {
+            "Status": "Failed",
+            "Stderr": "b'[ERROR]: Collision test 2 failed\\n'"
+          }
+        }
+      },
+      "MotionPlan_2DRRTConnect": {
+        "status": "PENDING",
+        "comments": [
+          "0 out of 4 test cases passed"
+        ],
+        "test results": {
+          "Test 0": {
+            "Status": "Failed",
+            "Stderr": "b'[ERROR]: Search time out\\n'"
+          },
+          "Test 1": {
+            "Status": "Failed",
+            "Stderr": "b'[ERROR]: Search time out\\n'"
+          },
+          "Test 2": {
+            "Status": "Failed",
+            "Stderr": "b'[ERROR]: Search time out\\n'"
+          },
+          "Test 3": {
+            "Status": "Failed",
+            "Stderr": "b'[ERROR]: Search time out\\n'"
+          }
+        }
+      },
+      "MotionPlan_CSpaceRRTConnect": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "Pitch": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "FK_BaseOffset": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      }
+    },
+    "quizzes": [
+      {
+        "status": "Takehome: 0.4 Interactive: 1.0"
+      },
+      {
+        "status": "Takehome: 0.5 Interactive: 1.0"
+      },
+      {
+        "status": "Takehome: 0.9 Interactive: 0.67"
+      },
+      {
+        "status": "Takehome (Doubled): 1.0  (No Interactive)"
+      },
+      {
+        "status": "Takehome: 0.25 Interactive: 0.4"
+      },
+      {
+        "status": "PENDING"
+      },
+      {
+        "status": "PENDING"
+      },
+      {
+        "status": "PENDING"
+      },
+      {
+        "status": "PENDING"
+      },
+      {
+        "status": "PENDING"
+      },
+      {
+        "status": "PENDING"
+      }
+    ]
+  }
+}
